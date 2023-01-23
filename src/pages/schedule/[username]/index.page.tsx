d26807cb2a8775f9f86d77c4045e15c8e900e72d
Pedro Gomes <pedro-gomes@ibm.com>
2023-01-23 17:57:30 -0300

    chore: added CalendarStep component render

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -1,6 +1,7 @@
 import { Avatar, Heading, Text } from "@ignite-ui/react";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { prisma } from "../../../lib/prisma";
+import { CalendarStep } from "./ScheduleForm/CalendarStep/index.page";
 import { Container, UserHeader } from "./styles";
 
 interface ScheduleProps {
@@ -19,6 +20,8 @@ export default function Schedule({ user }: ScheduleProps) {
         <Heading>{user.name}</Heading>
         <Text>{user.bio}</Text>
       </UserHeader>
+
+      <CalendarStep />
     </Container>
   );
 }
