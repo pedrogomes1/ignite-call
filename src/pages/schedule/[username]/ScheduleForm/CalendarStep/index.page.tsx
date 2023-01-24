439693a624978f412702596ef44e0ffc53fb5058
Pedro Gomes <pedro-gomes@ibm.com>
2023-01-24 16:05:52 -0300

    chore: create new function handleSelectTime and receive new prop onSelectDateTime

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.page.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.page.tsx
@@ -17,7 +17,11 @@ interface Availability {
   availableTimes: number[];
 }
 
-export function CalendarStep() {
+interface CalendarStepProps {
+  onSelectDateTime: (date: Date) => void;
+}
+
+export function CalendarStep({ onSelectDateTime }: CalendarStepProps) {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const router = useRouter();
 
@@ -34,7 +38,7 @@ export function CalendarStep() {
     : null;
 
   const { data: availability } = useQuery<Availability>(
-    ["availability", selectedDate],
+    ["availability", selectedDateWithoutTime],
     async () => {
       const response = await api.get(`users/${username}/availability`, {
         params: {
@@ -49,6 +53,15 @@ export function CalendarStep() {
     }
   );
 
+  function handleSelectTime(hour: number) {
+    const dateWithTime = dayjs(selectedDate)
+      .set("hour", hour)
+      .startOf("hour")
+      .toDate();
+
+    onSelectDateTime(dateWithTime);
+  }
+
   return (
     <Container isTimePickerOpen={hasSelectedDate}>
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
@@ -61,6 +74,7 @@ export function CalendarStep() {
             {availability?.possibleTimes.map((hour) => (
               <TimePickerItem
                 key={hour}
+                onClick={() => handleSelectTime(hour)}
                 disabled={!availability.availableTimes.includes(hour)}
               >
                 {String(hour).padStart(2, "0")}:00h
